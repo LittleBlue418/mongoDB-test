5acf67df3193bc9cf29a4eb02f23cb81c8474889
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-11 17:53:54 +0200

    find specific

--- mongo.py
+++ mongo.py
@@ -18,12 +18,12 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948', 'gender': 'm', 'hair_color': 'white', 'nationality': 'english', 'occupation': 'writer'},
-            {'first': 'george', 'last': 'rr martin', 'dob': '20/09/1948', 'gender': 'm', 'hair_color': 'white', 'nationality': 'american', 'occupation': 'writer'}]
+#new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948', 'gender': 'm', 'hair_color': 'white', 'nationality': 'english', 'occupation': 'writer'},
+#            {'first': 'george', 'last': 'rr martin', 'dob': '20/09/1948', 'gender': 'm', 'hair_color': 'white', 'nationality': 'american', 'occupation': 'writer'}]
 
-coll.insert_many(new_docs)
+#coll.insert_many(new_docs)
 
-documents = coll.find()
+documents = coll.find({'first':'douglas'})
 
 for doc in documents:
     print(doc)
